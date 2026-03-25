824a6a815cc2c6ce79db8491019df632f3a29529
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-25 09:47:40 +0100

    Add/update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-setup(
-    name="salud-mental-latinoamerica-prevalencia-recursos-brecha-tratamiento-2000-2024",
-    version="1.0.0",
-    description="Base de datos de salud mental en 10 países de América Latina (2000–2024): depresión, ansiedad, suici",
-    author="de la Serna, Juan Moisés",
-    url="https://github.com/juanmoisesd/salud-mental-latinoamerica-prevalencia-recursos-brecha-tratamiento-2000-2024",
-    packages=find_packages(),
-    install_requires=["pandas>=1.3.0","requests>=2.26.0"],
-    python_requires=">=3.7",
-    classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Topic :: Scientific/Engineering"],
-    keywords="zenodo, open-data",
-)
+from setuptools import setup,find_packages
+setup(name="salud-mental-latinoamerica-prevalencia-recursos-brecha-tratamiento-2000-2024",version="1.0.0",description="Base de datos de salud mental en 10 países de América Latina (2000–2024): depresión, ansiedad, suici" ,author="de la Serna, Juan Moisés",url="https://github.com/juanmoisesd/salud-mental-latinoamerica-prevalencia-recursos-brecha-tratamiento-2000-2024",packages=find_packages(),install_requires=["pandas>=1.3.0","requests>=2.26.0"],python_requires=">=3.7",keywords="zenodo, open-data")
